79dac5f82ca7103e8612199739712e2f1cd393c1
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-02-26 14:39:39 +0100

    rearranging to dynamic tables

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,6 @@ const db_name = process.env.DB_NAME
 const db_user = process.env.DB_USER
 const db_pass = process.env.DB_PASS
 
-const client = await connect({
-  host: db_host,
-  port: db_port,
-  user: db_user,
-  password: db_pass,
-  database: db_name
-})
-
 const smtp_host = process.env.SMTP_HOST
 const smtp_port = process.env.SMTP_PORT
 const smtp_user = process.env.SMTP_USER
@@ -25,38 +17,39 @@ const smtp_pass = process.env.SMTP_PASS
 const smtp_from = process.env.SMTP_FROM
 const smtp_secure = process.env.SMTP_SECURE
 
-const transporter = mailer.createTransport({
-  host: smtp_host,
-  port: smtp_port,
-  secure: smtp_secure,
-  auth: {
-    user: smtp_user,
-    pass: smtp_pass
-  }
-})
-
 try {
-  
-  const tables = [
-    "messages_05",
-    "messages_06",
-    "messages_07",
-    "messages_08",
-    "messages_09",
-    "messages_10",
-    "messages_11"
-  ]
 
-  for (const table of tables) {
-    const query = "SELECT id, to, subject, html_body FROM $1 WHERE id NOT IN (SELECT message_id FROM sent_emails WHERE email_table = $1)"
-    const result = await client.query(query, [table])
+  const client = await connect({
+    host: db_host,
+    port: db_port,
+    user: db_user,
+    password: db_pass,
+    database: db_name
+  })
+
+  const transporter = mailer.createTransport({
+    host: smtp_host,
+    port: smtp_port,
+    secure: smtp_secure,
+    auth: {
+      user: smtp_user,
+      pass: smtp_pass
+    }
+  })
+
+  const query1 = "SELECT table_name FROM information_schema.tables WHERE table_name ~ '^messages_(\d+)_(\d+)$'"
+  const tables = await client.query(query) 
+
+  for await (const table of tables) {
+    const query2 = "SELECT id, to, subject, html_body FROM $1 WHERE id NOT IN (SELECT message_id FROM sent_emails WHERE email_table = $1)"
+    const messages = await client.query(query, [table.table_name])
     
-    for await (const row of result) {
+    for await (const message of messages) {
       const options = {
         from: smtp_from,
-        to: row.to,
-        subject: row.subject,
-        html: row.html_body
+        to: message.to,
+        subject: message.subject,
+        html: message.html_body
       }
 
       console.log(options)
@@ -65,7 +58,7 @@ try {
           console.log(`Email sent: ${info.response}`)
   
           const insert = "INSERT INTO sent_emails (email_table, message_id) VALUES ($1, $2)"
-          client.query(insert, [table, row.id])
+          client.query(insert, [table.table_name, message.id])
       })*/
     }
 
