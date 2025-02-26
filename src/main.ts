da5a921f58ec8fb93a52d015fc989d2d9dfa9eb5
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-02-26 15:05:57 +0100

    fix logic errors

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,38 @@ import * as mailer from 'nodemailer'
 
 dotenv.config()
 
-const db_host = process.env.DB_HOST
-const db_port = process.env.DB_PORT
-const db_name = process.env.DB_NAME
-const db_user = process.env.DB_USER
-const db_pass = process.env.DB_PASS
+const db_host: string = process.env.DB_HOST
+const db_port: number = parseInt(process.env.DB_PORT)
+const db_name: string = process.env.DB_NAME
+const db_user: string = process.env.DB_USER
+const db_pass: string = process.env.DB_PASS
 
-const smtp_host = process.env.SMTP_HOST
-const smtp_port = process.env.SMTP_PORT
-const smtp_user = process.env.SMTP_USER
-const smtp_pass = process.env.SMTP_PASS
-const smtp_from = process.env.SMTP_FROM
-const smtp_secure = process.env.SMTP_SECURE
+const smtp_host: string = process.env.SMTP_HOST
+const smtp_port: number = parseInt(process.env.SMTP_PORT)
+const smtp_user: string = process.env.SMTP_USER
+const smtp_pass: string = process.env.SMTP_PASS
+const smtp_from: string = process.env.SMTP_FROM
+const smtp_secure: boolean = JSON.parse(process.env.SMTP_SECURE)
+
+const client = await connect({
+  host: db_host,
+  port: db_port,
+  user: db_user,
+  password: db_pass,
+  database: db_name
+})
+
+const transporter = mailer.createTransport({
+  host: smtp_host,
+  port: smtp_port,
+  secure: smtp_secure,
+  auth: {
+    user: smtp_user,
+    pass: smtp_pass
+  }
+})
 
 try {
-
-  const client = await connect({
-    host: db_host,
-    port: db_port,
-    user: db_user,
-    password: db_pass,
-    database: db_name
-  })
-
-  const transporter = mailer.createTransport({
-    host: smtp_host,
-    port: smtp_port,
-    secure: smtp_secure,
-    auth: {
-      user: smtp_user,
-      pass: smtp_pass
-    }
-  })
 
   const tquery = "SELECT table_name FROM information_schema.tables WHERE table_name ~ '^messages_(\d+)_(\d+)$'"
   const tables = await client.query(tquery) 
